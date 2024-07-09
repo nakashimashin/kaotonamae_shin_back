1ce439864a19b9515cd049a9bfb4b8a0ae9b56d4
nakashimashin <nakashimashin0517@gmail.com>
2024-07-10 00:09:54 +0900

    Hello World

--- main.go
+++ main.go
@@ -1,13 +1,18 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	// "net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
 
 func main() {
 	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "pong update",
+	r.GET("/", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "Hello World!",
 		})
 	})
-	r.Run()
+	r.Run(":8080")
 }
