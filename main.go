9d97b77023f1dad11185578c5a388f2944d7e13d
nakashimashin <nakashimashin0517@gmail.com>
2024-07-13 18:09:46 +0900

    MySQLと接続

--- main.go
+++ main.go
@@ -1,18 +1,39 @@
 package main
 
 import (
-	// "net/http"
+	"net/http"
+	"os"
+	"fmt"
 
 	"github.com/gin-gonic/gin"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
 )
 
+type Result struct {
+	ID int
+	Name string
+}
 
 func main() {
-	r := gin.Default()
-	r.GET("/", func(c *gin.Context) {
+	engine := gin.Default()
+	engine.GET("/", func(c *gin.Context) {
+		dsn := fmt.Sprintf("%s:%s@tcp(kaotonamae_db:3306)/%s?charset=utf8mb4&parseTime=True&loc=Local"
+			os.Getenv("DB_USER"),
+			os.Getenv("DB_PASSWORD"),
+			os.Getenv("DB_DATABASE"),
+		)
+		db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+		if err != nil {
+			panic("failed to connect database")
+		}
+
+		var result Result
+		db.First(&result)
+
 		c.JSON(http.StatusOK, gin.H{
-			"message": "Hello World!",
+			"test" : result.Name,
 		})
 	})
-	r.Run(":8080")
+	engine.Run(":8080")
 }
